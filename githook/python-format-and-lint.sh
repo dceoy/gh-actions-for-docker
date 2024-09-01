bda712f76134634d6050fa74718214e6359e0d0a
dceoy <dnarsil+github@gmail.com>
2024-09-02 00:51:47 +0900

    Update src/pyproject.toml

--- githook/python-format-and-lint.sh
+++ githook/python-format-and-lint.sh
@@ -2,7 +2,20 @@
 
 set -euox pipefail
 
-ruff format --exclude=build --line-length=79 . && isort --skip-glob=build .
-flake8 --exclude=build . && ruff check --exclude=build .
-mypy --exclude=build --install-types --non-interactive --ignore-missing-imports .
-bandit --exclude=build --recursive .
+package_directory="$(find . -type f -name 'pyproject.toml' -exec dirname {} \;)"
+if [[ -f "${package_directory}/poetry.lock" ]]; then
+  cd "${package_directory}"
+  poetry run ruff format .
+  poetry run isort .
+  poetry run flake8 .
+  poetry run ruff check .
+  poetry run mypy .
+  poetry run bandit .
+else
+  ruff format --exclude=build --line-length=79 .
+  isort --skip-glob=build .
+  flake8 --exclude=build .
+  ruff check --exclude=build .
+  mypy --exclude=build --install-types --non-interactive --ignore-missing-imports .
+  bandit --exclude=build --recursive .
+fi
